5af1b0cd58a92b16c9987a5d9f38ee2c743f8c04
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-19 13:09:24 -0500

    added folders

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,14 +2,18 @@ import homeIcon from '../images/icons8-home.svg';
 import spotIcon from '../images/icons8-animal-60.png';
 import porfileIcon from '../images/icons8-customer-26.png';
 import './Header.scss';
+import HomePage from '../HomePage/HomePage';
 
 function Header() {
     return (
+        <>
         <header className='navtab'>
             <img  className='icon-home'src={homeIcon}></img>
             <img className='icon-spot' src={spotIcon}></img>
             <img className='icon-porfile' src={porfileIcon}></img>
         </header>
+        <HomePage />
+        </>
     )
 }
 
---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import './HomePage.scss';
+function HomePage(){
+    return (
+        <main>
+            <div className='main-porfilePic'></div>
+            <img className='postPic'></img>
+            {/* <img src=' */}
+        </main>
+    )
+}
+
+export default HomePage;
