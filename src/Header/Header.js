4b8460a3f737e1836590006b90fbae66a732d892
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-20 15:53:04 -0500

    trying

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,14 +2,23 @@ import homeIcon from '../images/icons8-home.svg';
 import spotIcon from '../images/icons8-animal-60.png';
 import porfileIcon from '../images/icons8-customer-26.png';
 import './Header.scss';
+import { Link } from "react-router-dom";
+
+
 
 function Header() {
     return (
         <>
         <header className='navtab'>
-            <img  className='icon-home'src={homeIcon}></img>
-            <img className='icon-spot' src={spotIcon}></img>
-            <img className='icon-porfile' src={porfileIcon}></img>
+            <Link to={'/'}>
+                <img  className='icon-home'src={homeIcon}></img>
+            </Link>
+            <Link to={'/spot-light'}>
+                <img className='icon-spot' src={spotIcon}></img>
+            </Link>
+            <Link to={'/porfile'}>
+                <img className='icon-porfile' src={porfileIcon}></img>
+            </Link>
         </header>
         </>
     )
