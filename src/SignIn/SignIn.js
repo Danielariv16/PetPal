abb4d6972c689735e78b2353d93e3380b2794c81
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-21 15:50:16 -0500

    added style for login page

--- src/SignIn/SignIn.js
+++ src/SignIn/SignIn.js
@@ -1,8 +1,16 @@
 import './SignIn.scss';
+import logo from '../images/logo.png';
 
 function SignIn(){
     return(
         <>
+        <section className='sign_in'>
+            <img src={logo} className='sign_in-logo'></img>
+            <input name='email' className='sign_in-email' placeholder='Username, or email'></input>
+            <input name='password' className='sign_in-password' placeholder='Password'></input>
+            <button className='sign_in-logIn'>Log in</button>
+            <p className='sign_in-or'>━━━━━ or ━━━━━</p>
+        </section>
         </>
     )
 }
