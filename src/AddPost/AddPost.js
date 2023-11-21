007a280b397e0aaf0b72c6ee30eb7208ef14e894
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-21 14:45:56 -0500

    added back navigation

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -1,11 +1,24 @@
 import Header from '../Header/Header';
 import './AddPost.scss';
 import share from '../images/share.png';
+import arrow from '../images/arrow.png';
+
 
 function AddPost(){
+
+    const handleBackClick = () => {
+        window.history.back();
+          };
+
     return (
         <>
         <Header />
+        <div className='comments_back'>
+            <button className='button-back 'onClick={handleBackClick}>
+                <img className='comments_section-arrow'src={arrow} alt='back arrow'></img>
+            </button>
+            <p className='back'>Back</p>
+        </div>
         <section className='add_post'>
             <h6 className='add_post-addImg'>ADD YOUR IMAGE</h6>
             <h6 className='add_post-addDesc'>ADD DESCRIPTION</h6>
