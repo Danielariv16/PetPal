c848000d5bd2e369ee6a7d9fc5715071a964d784
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-21 14:38:11 -0500

    added simple styling, yet not finish

--- src/AddPost/AddPost.js
+++ src/AddPost/AddPost.js
@@ -1,8 +1,17 @@
+import Header from '../Header/Header';
 import './AddPost.scss';
+import share from '../images/share.png';
 
 function AddPost(){
     return (
         <>
+        <Header />
+        <section className='add_post'>
+            <h6 className='add_post-addImg'>ADD YOUR IMAGE</h6>
+            <h6 className='add_post-addDesc'>ADD DESCRIPTION</h6>
+            <button className='add_post-share'>Share <img className='share-image'src={share}></img> </button>
+
+        </section>
         </>
     )
 }
