e196fd0266fb238c2fc98b863f2c7df873b21d2e
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-24 13:33:19 -0500

    homepage display all images that were post

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,13 +4,45 @@ import heartIcon from '../images/heartLike.png';
 import commentIcon from '../images/comment.png';
 import Header from '../Header/Header';
 import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+import { storage } from '../../config/firebase';
+
+
 
 function HomePage(){
+
+    const imagesFolder = ref(storage, 'imagesHome');
+    
+    const [images, setImages] = useState([]);
+
+
+    useEffect(() => {
+        const getImages = async () => {
+            try {
+                const response = await listAll(imagesFolder);
+                const urls = [];
+    
+                for (const item of response.items) {
+                    const url = await getDownloadURL(item);
+                    urls.push(url);
+                }
+    
+                setImages(urls);
+            } catch (error) {
+                console.error('Error getting images:');
+            }
+        };
+    
+        getImages();
+    }, []);
+    
+
     return (
         <>
             <Header />
             <main className='HomePage'>
-                <div className='main-picName'>
+                {/* <div className='main-picName'>
                     <div className='main-porfilePic'></div>
                     <h6 className='porfileName'>Name</h6>
                 </div>
@@ -20,7 +52,24 @@ function HomePage(){
                     <Link to={'/comments'}>
                         <img className='comment' src={commentIcon}></img>
                     </Link>
-                </div>
+                </div> */}
+                {
+                    images?.map((url) =>(
+                        <>
+                        <div className='main-picName'>
+                        <div className='main-porfilePic'></div>
+                        <h6 className='porfileName'>Name</h6>
+                    </div>
+                    <img className='postPic' src={url}></img>
+                    <div className='reaction-container'>
+                        <img className='like' src={heartIcon}></img>
+                        <Link to={'/comments'}>
+                            <img className='comment' src={commentIcon}></img>
+                        </Link>
+                    </div>
+                        </>
+                    ))
+                }
             </main>
         
         </>
