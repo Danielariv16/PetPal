01b6329dd72b7addd008ac93e0f8d5067fc9ab76
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-24 12:51:46 -0500

    added function to be able to upload an image

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -2,9 +2,25 @@ import Header from '../Header/Header';
 import './AddPost.scss';
 import share from '../images/share.png';
 import arrow from '../images/arrow.png';
-
+import { storage } from '../../config/firebase';
+import { ref, uploadBytes } from "firebase/storage";
+import { useState} from 'react'
+import {v4} from 'uuid'
 
 function AddPost(){
+    const [uploadImage, setUploadImage] =  useState(null);
+
+    const imageUpload = () => {
+        if(uploadImage == null) return
+
+        const homeImagesRef = ref(storage, `imagesHome/ ${uploadImage.name + v4()}`)
+        try {
+            uploadBytes(homeImagesRef, uploadImage)}
+        catch (err){
+            console.error(err)
+        }
+        }
+    ;
 
     const handleBackClick = () => {
         window.history.back();
@@ -15,14 +31,20 @@ function AddPost(){
         <Header />
         <div className='comments_back'>
             <button className='button-back 'onClick={handleBackClick}>
-                <img className='comments_section-arrow'src={arrow} alt='back arrow'></img>
+                <img className='comments_section-arrow'src={arrow}
+                    alt='back arrow'>
+                 </img>
             </button>
             <p className='back'>Back</p>
         </div>
+
         <section className='add_post'>
             <h6 className='add_post-addImg'>ADD YOUR IMAGE</h6>
+            <input type='file' className='upload-image' 
+                onChange={(e) => {setUploadImage(e.target.files[0])}}>
+            </input>
             <h6 className='add_post-addDesc'>ADD DESCRIPTION</h6>
-            <button className='add_post-share'>Share <img className='share-image'src={share}></img> </button>
+            <button className='add_post-share'onClick={imageUpload}>Share <img src={share} className='share-image'></img></button>
 
         </section>
         </>
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyBUVnw9mjO342kuKitAw2kILwyIf-sGrDo",
@@ -16,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider =  new GoogleAuthProvider();
 export const db = getFirestore(app)
+export const storage = getStorage(app)
