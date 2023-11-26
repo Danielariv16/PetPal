912dfcf392b7d847c23d87e42e14d33a866ea5f0
danielariveramya@gmail.com <danielariveramya@gmail.com>
2023-11-26 14:07:20 -0500

    added style for signIn page

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -5,6 +5,9 @@ import { signInWithEmailAndPassword, signInWithPopup} from 'firebase/auth';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import {collection, addDoc} from 'firebase/firestore';
+import googleIcon from '../../images/google-icon.jpg';
+import { Link } from "react-router-dom";
+
 
 
 
@@ -14,8 +17,6 @@ function SignIn(){
     const navigate = useNavigate();
 
     const usersCollection = collection(db , 'users')
-
-  
 
 
 
@@ -85,9 +86,23 @@ function SignIn(){
             onClick={signIn}>Log in</button>
             <p className='sign_in-or'>━━━━━ or ━━━━━</p>
             <div className='sign_in-google'>
-                <button className='sign_in-question' onClick={loginWithGoogle}>Log in with Google</button>
+                <div className='google-signIn-container' 
+                onClick={loginWithGoogle}>
+                    <img src={googleIcon} className='googleIcon'>
+                    </img>
+                    Log in with Google
+                </div>
             </div>
             
+            <div className='sign_in-up'>
+                <p className='sign_in-option2'>
+                    Don't have an account?
+                </p>
+                <Link to={'/sign-up'}>
+                    <p className='sign-up-link'>Sign up</p>
+                
+                </Link>
+            </div>
         </section>
         </>
     )
